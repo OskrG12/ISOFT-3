8f35c5d62e578bbbddeb2459171fc8f86db1c182
OskrG12 <106770962+OskrG12@users.noreply.github.com>
2022-10-18 13:13:01 -0500

    Add files via upload
    Proyecto realizado en clase haciendo uso de la herramienta dialogo de GODOT

--- Jugador.gd
+++ Jugador.gd
@@ -1,20 +1,21 @@
 extends KinematicBody2D
 
-var velocidad:int = 400
-var brinco:int = 1000
-var gravedad:int = 800
-var vel:Vector2 = Vector2()
-var piso:bool = false
-onready var sprite = $Sprite
+var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-	vel.x = 0
-	if Input.is_action_pressed("ui_left"):
-		vel.x -= velocidad
-	if Input.is_action_pressed("ui_right"):
-		vel.x += velocidad
-	vel = move_and_slide(vel,Vector2.UP)
-	vel.y += gravedad * delta
-	if Input.is_action_pressed("ui_up") and is_on_floor():
-		vel.y -= brinco
-	pass
+	get_input()
+	velocity = move_and_slide(velocity)
